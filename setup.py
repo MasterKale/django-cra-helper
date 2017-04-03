82189ccb271256d896a2d719b8139f1f612395b2
Matthew Miller <mmiller@dhapdigital.com>
2017-04-03 00:42:24 -0700

    Added files for PyPI deployment

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'django-cra-helper',
+    packages = ['cra_helper'],
+    version = '1.0',
+    description = 'The missing piece of the Django + React puzzle',
+    author = 'Matthew Miller',
+    author_email = 'kale@iammiller.com',
+    url = 'https://github.com/MasterKale/django-cra-helper',
+    download_url = 'https://github.com/MasterKale/django-cra-helper/archive/1.0.tar.gz',
+    keywords = ['django', 'react', 'create-react-app', 'integrate'],
+    classifiers = [],
+)
