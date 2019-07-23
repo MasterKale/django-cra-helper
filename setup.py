e1c8165351ef5083328df8fe36fb062bc1e398ab
Matthew Miller <matthew@millerti.me>
2019-07-22 20:32:31 -0700

    Bump version to v1.0.2

--- setup.py
+++ setup.py
@@ -5,24 +5,28 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name = 'django-cra-helper',
-    version = '1.0.1',
-    description = 'The missing piece of the Django + React puzzle',
-    long_description = 'A Django app that allows you to easily incorporate create-react-app code in your Django project',
-    url = 'https://github.com/MasterKale/django-cra-helper',
-    author = 'Matthew Miller',
-    author_email = 'kale@iammiller.com',
-    license = 'MIT',
-    classifiers = [
+    name='django-cra-helper',
+    version='1.0.2',
+    description='The missing piece of the Django + React puzzle',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/MasterKale/django-cra-helper',
+    author='Matthew Miller',
+    author_email='matthew@millerti.me',
+    license='MIT',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
     ],
-    keywords = 'django react create-react-app integrate',
-    packages = find_packages(exclude=['contrib', 'docs', 'tests']),
-    install_requires = [
+    keywords='django react create-react-app integrate',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[
         'bleach>=2'
     ],
 )
