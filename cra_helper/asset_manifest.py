b3246016d4d9fccce852d43997078ca3457c94bd
Matthew Miller <matthew@millerti.me>
2019-10-11 17:17:09 -0700

    Update asset manifest parsing for v3+ support

--- cra_helper/asset_manifest.py
+++ cra_helper/asset_manifest.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import json
+import re
 
 from django.conf import settings
 
@@ -9,6 +10,16 @@ logger = logging.getLogger(__name__)
 _asset_filename = 'asset-manifest.json'
 
 
+# Sanitize characters that Django's `static` template tag can't handle
+def clean_file_key(filename: str) -> str:
+    cleaned = filename.replace('.', '_')
+    cleaned = cleaned.replace('/', '_')
+    cleaned = cleaned.replace('~', '_')
+    cleaned = cleaned.replace('-', '_')
+    return cleaned
+
+# Create a dictionary of keys that can be passed to the `static` template tag to load CRA assets
+# from Django's STATIC_ROOT directory
 def generate_manifest(is_server_live: bool, bundle_path: str, app_dir: str) -> dict:
     # Prepare references to various files frontend
     if is_server_live:
@@ -42,18 +53,47 @@ def generate_manifest(is_server_live: bool, bundle_path: str, app_dir: str) -> d
                 logger.error('can\'t load static asset manifest: {}'.format(e))
                 return {}
 
-        # Older versions of Create-React-App had flat manifests. If a "files" property doesn't
-        # exist in the manifest JSON, then we're probably working with an older version of CRA
+        # Older versions of Create-React-App (through v2.1.8) had flat manifests. If a "files"
+        # property doesn't exist in the manifest JSON, then we're probably working with an older
+        # version of CRA.
         if data.get('files') is None:
             manifest_items = data.items()
         else:
             manifest_items = data.get('files').items()
 
-        # Generate relative paths to our bundled assets
-        for filename, path in manifest_items:
-            asset_key = filename.replace('.', '_')
-            asset_key = asset_key.replace('/', '_')
+        # Prepare a regex that'll help us detect and remove the leading "/static/" from manifest
+        # filepaths
+        static_base_path = re.compile(r'^/static/', re.IGNORECASE)
+        for file_key, path in manifest_items:
+            # Don't include index.html, serviceWorker.js, etc...
+            if static_base_path.match(path):
+                # Generate paths relative to our bundled assets
+                # Ex: /static/css/main.99358b65.chunk.css => css/main.99358b65.chunk.css
+                _manifest[clean_file_key(file_key)] = re.sub(static_base_path, '', path)
 
-            _manifest[asset_key] = os.path.relpath(path, 'static/')
+        # Later versions of Create-React-App (starting with v3.2.0) will tell us which files to
+        # load via an "entrypoints" array
+        entrypoints = data.get('entrypoints')
+        if entrypoints is not None:
+            # Prepare to group entrypoint files by extension
+            _manifest['entrypoints'] = {
+                'js': [],
+                'css': [],
+            };
+
+            # Map manifest files by their path
+            mapped_manifest_items = {}
+            for file_key, path in manifest_items:
+                # Paths in "entrypoints" don't start with a leading "/" so we have to trim it off
+                # with the `[1:]`
+                mapped_manifest_items[path[1:]] = clean_file_key(file_key)
+
+            for path in entrypoints:
+                rel_static_path = _manifest[mapped_manifest_items[path]]
+
+                if path.endswith('.js'):
+                    _manifest['entrypoints']['js'].append(rel_static_path)
+                elif path.endswith('.css'):
+                    _manifest['entrypoints']['css'].append(rel_static_path)
 
         return _manifest
