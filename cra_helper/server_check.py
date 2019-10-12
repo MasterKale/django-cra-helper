fb8138caaaade04145ad4619f8ed059dfa21a399
Matthew Miller <matthew@millerti.me>
2019-10-11 20:41:52 -0700

    Rename `is_server_live to `hosted_by_liveserver`

--- cra_helper/server_check.py
+++ cra_helper/server_check.py
@@ -4,16 +4,16 @@ from urllib import request, error as url_error
 from django.conf import settings
 
 
-def is_server_live(server_path: str) -> bool:
+def hosted_by_liveserver(file_url: str) -> bool:
     # Ignore the server check if we're in production
     if settings.DEBUG:
         try:
-            resp = request.urlopen(server_path)
+            resp = request.urlopen(file_url)
             if resp.status == 200:
                 logging.info('CRA liveserver is running')
                 return True
             else:
-                logging.warning('CRA liveserver is up but not serving bundle.js')
+                logging.warning('CRA liveserver is up but not serving files')
                 return False
         except url_error.URLError as err:
             logging.warning('CRA liveserver is not running')
