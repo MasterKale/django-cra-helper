e54944e201086226b5a4a46164d656b4a06c0619
Matthew Miller <matthew@millerti.me>
2019-10-11 20:43:29 -0700

    Fix hosted_by_liveserver unittests

--- cra_helper/tests/test_server_check.py
+++ cra_helper/tests/test_server_check.py
@@ -11,30 +11,30 @@ from cra_helper import CRA_URL, server_check
 logging.disable(logging.CRITICAL)
 
 
-class TestIsServerAlive(TestCase):
+class TestHostedByLiveserver(TestCase):
     def setUp(self):
         setattr(settings, 'DEBUG', True)
 
     def test_returns_boolean(self):
-        self.assertIsInstance(server_check.is_server_live(CRA_URL), bool)
+        self.assertIsInstance(server_check.hosted_by_liveserver(CRA_URL), bool)
 
     @patch('urllib.request.urlopen')
     def test_returns_true_when_cra_running_and_debug(self, mock_urlopen):
         mock_urlopen.return_value = MagicMock(status=200)
-        self.assertTrue(server_check.is_server_live(CRA_URL))
+        self.assertTrue(server_check.hosted_by_liveserver(CRA_URL))
 
     @patch('urllib.request.urlopen')
     def test_returns_false_when_cra_running_and_production(self, mock_urlopen):
         mock_urlopen.return_value = MagicMock(status=200)
         setattr(settings, 'DEBUG', False)
-        self.assertFalse(server_check.is_server_live(CRA_URL))
+        self.assertFalse(server_check.hosted_by_liveserver(CRA_URL))
 
     @patch('urllib.request.urlopen')
     def test_returns_false_when_cra_errors(self, mock_urlopen):
         mock_urlopen.return_value = MagicMock(status=500)
-        self.assertFalse(server_check.is_server_live(CRA_URL))
+        self.assertFalse(server_check.hosted_by_liveserver(CRA_URL))
 
     @patch('urllib.request.urlopen')
     def test_returns_false_when_cra_not_running(self, mock_urlopen):
         mock_urlopen.side_effect = url_error.URLError('CRA not running')
-        self.assertFalse(server_check.is_server_live(CRA_URL))
+        self.assertFalse(server_check.hosted_by_liveserver(CRA_URL))
